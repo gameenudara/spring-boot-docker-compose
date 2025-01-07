633ac4c06210173ba36f267fee245726e2cc26df
gameenudara <gameenudara567@gmail.com>
2025-01-07 10:51:59 +0530

    git init

---
+++ spring-boot-docker-compose/src/test/java/lk/docker/compose/api/SpringBootDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.docker.compose.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
